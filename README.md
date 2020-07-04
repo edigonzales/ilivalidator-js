46a3315c27ed211809c1c9805f7f2bc0a3db47b1
Stefan Ziegler <edi.gonzales@gmail.com>
2020-07-04 19:25:32 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ilivalidator-js
+
+Initialize node app and install some modules:
+```
+npm init
+npm install express multer --save
+```
+
+Run ilivalidator-web-service:
+```
+node --jvm --vm.cp ilivalidator-1.11.7-SNAPSHOT-all.jar app.js
+```
+
+`ilivalidator-1.11.7-SNAPSHOT-all.jar` is a fat jar of ilivalidator.
--- app.js
+++ app.js
@@ -1,14 +1,60 @@
-const http = require('http');
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const multer = require('multer');
+const path = require('path');
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+// TODO: Ist nicht thread safe.
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+const SettingsJavaClass = Java.type('ch.ehi.basics.settings.Settings');
+const settings = new SettingsJavaClass();
+const ValidatorJavaClass = Java.type('org.interlis2.validator.Validator');
+
+app.get('/fubar', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+    let upload = multer({ storage: storage }).single('file');
+
+    upload(req, res, function(err) {
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Not a file.');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        settings.setValue(ValidatorJavaClass.SETTING_ILIDIRS, ValidatorJavaClass.SETTING_DEFAULT_ILIDIRS);
+        // TODO: Ist nicht thread safe.
+        var logFileName = "logs/" + req.file.originalname + ".log";
+        settings.setValue(ValidatorJavaClass.SETTING_LOGFILE, logFileName);
+
+        ValidatorJavaClass.runValidation("uploads/" + req.file.originalname, settings);
+        var content = fs.readFileSync(logFileName, 'utf8');
+
+        res.set('Content-Type', 'text/plain');
+        res.send(content);
+    });
+});
+
+app.listen(3000, function () {
+    console.log('App listening on port 3000!');
 });
